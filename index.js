03ecacf82cbc1e3430a249c40af8c28dd118e2a6
lathisha <lathisha.n@gmail.com>
2020-05-29 13:04:27 -0700

    installed npm

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const generateMarkdown = require("./utils/generateMarkdown")
+
 const questions = [
 
 ];
